c0390db385d84e4ad20cd333d98753df43240223
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-27 17:07:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function soundBar(){
+	   const btnZero = document.querySelector("#btn0");  
+	   const btnOne = document.querySelector("#btn1");
+	   const btnTwo = document.querySelector("#btn2");
+	   const btnThree = document.querySelector("#btn3");
+	   const btnFour = document.querySelector("#btn4");
+	   const btnFive = document.querySelector("#btn5");
+	   const btnSix = document.querySelector("#btn6");
+	   
+
+        btnOne.addEventListener("click", function(){
+			
+		});
+
+
+}
