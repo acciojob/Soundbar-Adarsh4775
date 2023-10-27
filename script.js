f35de08158c7ee4a652389f02d512a0a2b25ce87
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-27 19:31:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,10 +8,68 @@ function soundBar(){
 	   const btnFive = document.querySelector("#btn5");
 	   const btnSix = document.querySelector("#btn6");
 	   
-
-        btnOne.addEventListener("click", function(){
-			
+         
+        btnZero.addEventListener("click", function() {
+			var firstMusic = document.getElementById("audio1");
+			firstMusic.play();
+		});
+        
+	btnOne.addEventListener("click", function() {
+			var SecondMusic = document.getElementById("audio2");
+			SecondMusic.play();
+		});
+	btnTwo.addEventListener("click", function() {
+			var ThirdMusic = document.getElementById("audio3");
+			ThirdMusic.play();
+		});
+	btnThree.addEventListener("click", function() {
+			var FourthMusic = document.getElementById("audio4");
+			FourthMusic.play();
 		});
 
+	btnFour.addEventListener("click", function() {
+			var FifthMusic = document.getElementById("audio5");
+			FifthMusic.play();
+		});
+	btnFive.addEventListener("click", function() {
+			var SixthMusic = document.getElementById("audio6");
+			SixthMusic.play();
+		});
+	btnSix.addEventListener("click", function() {
+			var SeventhMusic = document.getElementById("audio7");
+			SeventhMusic.play();
+		});
 
+function playSound(soundFile) {
+  const sound = new Audio("https://www.computerhope.com/jargon/m/example.mp3");
+  sound.play();
+
+function playSound(soundFile) {
+  const sound = new Audio("https://www.computerhope.com/jargon/m/example.mp3");
+  sound.play();
+	
+	function playSound(soundFile) {
+  const sound = new Audio("https://www.computerhope.com/jargon/m/example.mp3");
+  sound.play();
+	function playSound(soundFile) {
+  const sound = new Audio("https://www.computerhope.com/jargon/m/example.mp3");
+  sound.play();
+	function playSound(soundFile) {
+  const sound = new Audio("https://www.computerhope.com/jargon/m/example.mp3");
+  sound.play();
+	function playSound(soundFile) {
+  const sound = new Audio("https://www.computerhope.com/jargon/m/example.mp3");
+  sound.play();
+	function playSound(soundFile) {
+  const sound = new Audio("https://www.computerhope.com/jargon/m/example.mp3");
+  sound.play();
 }
+}
+}
+}
+}
+}
+}
+}
+soundBar();
+
